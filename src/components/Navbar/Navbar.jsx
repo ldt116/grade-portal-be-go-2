e9ea86cf82fdd5f3ac375ee5665979f63dc4d513
tuvo3105 <tu.vo3105@hcmut.edu.vn>
2024-11-08 21:19:05 +0700

    Update student interface

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import arrowdown from "../../assets/img/chevron-down.svg"
 function Navbar() {
   return (
-    <div className="bg-white h-14 w-full flex border-b text-xl font-bold items-center px-20 z-40">
+    <div className="bg-white h-14 w-full flex border-b text-xl font-bold items-center px-20 z-40 fixed top-[109px]">
       <div className="px-10 cursor-pointer hover:bg-[#c3c3c3] h-full leading-[52px]">Trang chủ</div>
       <div className="px-10 cursor-pointer hover:bg-[#c3c3c3] h-full leading-[52px]">Tra cứu điểm</div>
       <div className="px-10 cursor-pointer hover:bg-[#c3c3c3] h-full leading-[52px] flex items-center">
