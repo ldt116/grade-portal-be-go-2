62c3b9ef0aa008e21ef48538d8d4f66b3087593c
hdanhng <danh.nguyenhoang@hcmut.edu.vn>
2024-11-09 16:32:42 +0700

    build: headerfooter

---
+++ src/components/HeaderFooter/Footer.tsx
@@ -0,0 +1,12 @@
+// Footer.tsx
+import React from 'react';
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = () => {
+  return (
+    <footer className="flex w-full bg-zinc-600 min-h-[240px] max-md:max-w-full" />
+  );
+};
+
+export default Footer;
