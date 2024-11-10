d4eecbefcf97af2a9a370ba0c563342abfc7edb9
why-am-i-still-single <118999153+why-am-i-still-single@users.noreply.github.com>
2024-11-10 21:31:36 +0700

    Add files via upload

---
+++ Table.jsx
@@ -0,0 +1,21 @@
+function GradeTable() {
+  return (
+    <div>
+      <table className="bg-[#ECE8E8] w-full">
+        <tr className="bg-[#0388B4]">
+          <th>Mã Lớp</th>
+          <th className="border-x border-white">Mã Môn Học</th>
+          <th className="border-x border-white">Tên Môn Học</th>
+          <th className="border-x border-white">Giảng Viên Phụ Trách</th>
+          <th className="border-x border-white">Số Lượng Thành Viên</th>
+          <th>Ngày Bắt Đầu</th>
+        </tr>
+        <tr>
+          
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default GradeTable
