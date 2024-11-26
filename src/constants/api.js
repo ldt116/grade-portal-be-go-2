b5cd6d2abca2c7292aab66e6da584f26720352d6
tuvo3105 <tu.vo3105@hcmut.edu.vn>
2024-11-26 10:42:30 +0700

    temp login

--- src/constants/api.js
+++ src/constants/api.js
@@ -12,4 +12,4 @@ export const GRADE_DETAIL_API_URL = (id) => `${CLIENT_API_URL}/resultScore/${id}
 export const TEACHER_DETAIL_API_URL = (id) => `${CLIENT_API_URL}/${id}`
 export const COURSE_DETAIL_API_URL = (id) => `${CLIENT_API_URL}/course/${id}`
 //----End API for Class List of Student----//
-export const ADMIN_LOGIN = `${ADMIN_API_URL}/login`
+export const ADMIN_LOGIN = `${CLIENT_API_URL}/login`
--- src/pages/LoginPages/Login.jsx
+++ src/pages/LoginPages/Login.jsx
@@ -1,9 +1,9 @@
 import logo from '../../assets/img/logo-notext.svg';
 import { ADMIN_LOGIN } from '../../constants/api.js'
 function Login() {
-    handleLogin = () => {
-        fetch(ADMIN_LOGIN) 
-            .then(res) { console.log(res);}
+    const handleLogin = () => {
+        fetch(ADMIN_LOGIN, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({idToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjM2MjgyNTg2MDExMTNlNjU3NmE0NTMzNzM2NWZlOGI4OTczZDE2NzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4NjY4MzQxNTc5Ny1hcTZuNzRqOWdka3JkN3BkM3U2YTJkNTVmaDU4N2NkMy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6Ijg2NjgzNDE1Nzk3LWFxNm43NGo5Z2RrcmQ3cGQzdTZhMmQ1NWZoNTg3Y2QzLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE2MjI1OTI1ODY1NjM1NjgzNTUwIiwiaGQiOiJoY211dC5lZHUudm4iLCJlbWFpbCI6ImRhbmcubmd1eWVuMjIxMDczN2NzQGhjbXV0LmVkdS52biIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYmYiOjE3MzI1NTAyOTgsIm5hbWUiOiLEkMSCTkcgTkdVWeG7hE4gSFXhu7JOSCBI4bqiSSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NJSnFDNTV3a3JadWx1amhzWldhdlA4eE5xaFRYTUltSVhEZTEyNTRkVmJZd3U4UFV3PXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IsSQxIJORyIsImZhbWlseV9uYW1lIjoiTkdVWeG7hE4gSFXhu7JOSCBI4bqiSSIsImlhdCI6MTczMjU1MDU5OCwiZXhwIjoxNzMyNTU0MTk4LCJqdGkiOiI3MmFjMzAwMWMxMzJhYTE5YzMzZjhkNzBlZmRmOGNkYTNiZjJmYTZlIn0.cdHC2U6uqyWK0Vx2u-AwDR1ohrowMw7t6xABmo-O3xab1p5obO2MD6n7K6U34nH2fmwL4Nibo28f0ZMupu3QQK9cwrgn479RF0dMvI6AQdSmSo55Vpr1vW49JTqAt2JvReY--pUsQCO1Hu2U48-fI-62RHe1m_pIeILv44fkb_XAT5LW6N4yiTzRRr1dhZOIi4q6445f1TWM0UisREn_rWhZMLhjIpiF0yxAca6xhBh2vsha1g-dq80pyQW8_Qg43Bq5X4-dSFtilNAImdoByoSldXa27dalEjczbpGWjPnFwhVI2C-LNSpHLYabL-VzM4g9NeAomY45yn0O9Aeh8Q'}) })
+        .then(response => console.log(response))
 }
     return (
         <div className="mt-[7rem] flex justify-center items-center py-20">
