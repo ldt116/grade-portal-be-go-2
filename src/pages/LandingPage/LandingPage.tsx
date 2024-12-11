9130fe4dd3c5b2734732a8b131990861616f3d01
DNguyen143 <dang.nguyen2210737cs@hcmut.edu.vn>
2024-12-11 23:54:13 +0700

    fixed

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,6 @@
 ﻿import landingImg from '../../assets/img/land-img.svg';
-
-
 function LandingPage() {
+
     return (
         <div className="mt-[7rem]">
             <img src={landingImg} alt='landing-img' className='w-full'></img>
